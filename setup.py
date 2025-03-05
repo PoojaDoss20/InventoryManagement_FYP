c6710afda12fce639889aa835a4a821378dfa96d
PoojaDoss20 <poojad5858@gmail.com>
2025-03-05 12:59:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='InventoryManagement_FYP',
+    version='0.0.1',
+    description='Inventory Management App for ERPNext',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[]
+)
